71ac892e5e5a5c9b66d26b3f4b06c137a8120c48
KonradBorowik <konrad1borowik@gmail.com>
2024-01-29 22:05:10 +0100

    check if child categories are empty before deletion

--- routes/categories.py
+++ routes/categories.py
@@ -4,21 +4,16 @@ from bson import ObjectId
 from models.category import Category
 from config.setup import client
 from schemas.category import categoryEntity, categoriesEntity
-from routes.utils.category_utils import check_if_category_is_empty
-
+from routes.utils.category_utils import (
+    check_if_category_is_empty,
+    check_if_child_catogories_are_empty
+)
 
 category = APIRouter()
 
 @category.get('/')
 async def find_all_categories():
     return categoriesEntity(client.konrad_borowik.categories.find())
-
-
-# @category.get('/{key: value}')
-# async def find_category_by_value(value: str):
-#     return categoryEntity(client.konrad_borowik.categories.find_one(
-#         {}, {value}
-#     ))
 
 
 @category.post('/')
@@ -40,22 +35,9 @@ async def update_whole_category(id, category: Category):
     return categoryEntity(client.konrad_borowik.categories.find_one({"_id": ObjectId(id)}))
 
 
-# @category.put('/{key: value}')
-# async def update_one_element_in_category(key: str, value: str, new_value: str):
-#     client.konrad_borowik.categories.find_one_and_update(
-#         {
-#             key: value
-#         },
-#         {
-#             "$set": {key: new_value} 
-#         }
-#     )
-#     return categoryEntity(client.konrad_borowik.categories.find())
-
-
 @category.delete('/{id}')
 async def delete_category(id):
-    if check_if_category_is_empty(client, id):
+    if check_if_category_is_empty(client, id) and check_if_child_catogories_are_empty(client, id):
         client.konrad_borowik.categories.find_one_and_delete({"_id": ObjectId(id)})
         return f'Category deleted.'
     else:
--- routes/utils/category_utils.py
+++ routes/utils/category_utils.py
@@ -1,7 +1,5 @@
 from bson import ObjectId
 
-from models.category import Category
-
 
 def check_if_category_is_empty(client, id: str) -> bool:
     category = client.konrad_borowik.categories.find_one({"_id": ObjectId(id)})
@@ -17,3 +15,20 @@ def check_if_category_is_empty(client, id: str) -> bool:
     if len(list(parts)) > 0:
         return False
     return True
+
+
+def check_if_child_catogories_are_empty(client, id: str) -> bool:
+    category = client.konrad_borowik.categories.find_one({"_id": ObjectId(id)})
+
+    sub_categories = client.konrad_borowik.categories.aggregate([
+        {
+            '$match': {
+                'parent_name': category['name']
+            }
+        }
+    ])
+    output = False
+    for sc in sub_categories:
+        output = check_if_category_is_empty(client, sc['_id'])
+    return output
+        

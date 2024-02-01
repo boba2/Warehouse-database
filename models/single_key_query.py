6595add4a13ce05525b98306173adf110dbdadc6
KonradBorowik <konrad1borowik@gmail.com>
2024-02-01 22:51:50 +0100

    not useful

--- models/single_key_query.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Union
-
-from models.part import Location
-
-
-class singleKeyQuery(BaseModel):
-    key: str
-    value: int | float | str | Location

b36d62bbb3b0d78a783058a2b877be64fc7a21f6
KonradBorowik <konrad1borowik@gmail.com>
2024-02-02 13:50:29 +0100

    connect to mongodb srv

--- config/setup.py
+++ config/setup.py
@@ -12,11 +12,13 @@ def read_file(filename: str) -> List:
 
 dummy_categories = read_file('dummy_categories.json')
 dummy_parts = read_file('dummy_parts.json')
-# mongodb+srv://rekrutacja:BZijftwEru0oELxT@cluster11.yxu8n2k.mongodb.net/
 client = MongoClient(
-    host='localhost',
-    port=27017,
+    'mongodb+srv://rekrutacja:BZijftwEru0oELxT@cluster11.yxu8n2k.mongodb.net/'
 )
+# client = MongoClient(
+#     host='localhost',
+#     port=27017,
+# )
 
 if not 'konrad_borowik' in client.list_database_names():
     db = client['konrad_borowik']
